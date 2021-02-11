c3c55c8bb479171acd11b8129cd7fb5bfb44004b
SimonaSriubaite <simona.sriubaite@gmail.com>
2021-02-11 10:51:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { ReactComponent as SourceryLogo } from "assets/logo.svg";
+
+function App() {
+  const [instructions, setInstructions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3008/instructions")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setInstructions(result);
+        },
+        (error) => {
+          // handle error here
+        }
+      );
+  }, []);
+
+  return (
+    <div className="app">
+      <header className="App-header">
+        <SourceryLogo />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
