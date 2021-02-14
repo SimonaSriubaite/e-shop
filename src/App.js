d1cccd43c09df78fe58925937595bf74f4fb8f9b
SimonaSriubaite <simona.sriubaite@gmail.com>
2021-02-14 11:29:38 +0200

    contexts added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Loader, Header, Footer } from "./components";
+import CartProvider from "./contexts/CartContext";
+import { DisplayCartProvider } from "./contexts/displayCart.context";
 import "./styles/index.scss";
 
 const HomeLazy = lazy(() => import("./pages/Home/Home"));
@@ -8,13 +10,17 @@ const HomeLazy = lazy(() => import("./pages/Home/Home"));
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Suspense fallback={<Loader />}>
-        <Switch>
-          <Route exact path="/" component={HomeLazy} />
-        </Switch>
-      </Suspense>
-      <Footer />
+      <CartProvider>
+        <DisplayCartProvider>
+          <Header />
+          <Suspense fallback={<Loader />}>
+            <Switch>
+              <Route exact path="/" component={HomeLazy} />
+            </Switch>
+          </Suspense>
+          <Footer />
+        </DisplayCartProvider>
+      </CartProvider>
     </div>
   );
 }
